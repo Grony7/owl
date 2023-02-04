9d996566b70d9d5c02bd1c13b3234c81e0c267d1
Grony <gr0ny.0017@gmail.com>
2023-02-04 03:42:33 +0300

    Добавляет компоненты

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css';
 import {AppProps} from "next/app";
+import Head from 'next/head';
 
 export default function App({Component, pageProps}: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Owl</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
