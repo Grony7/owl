85f4fda46f356821dd6a529b58b98935f300226e
Grony <gr0ny.0017@gmail.com>
2023-02-17 01:05:02 +0300

    menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,9 +26,6 @@ function Home({menu} : HomeProps): JSX.Element {
       <Tag href={"/order"}>Ссылка</Tag>
       <Tag color={'red'}>Кнопка</Tag>
       <Rating rating={rating} setRating={setRating} isEditable/>
-      <ul>
-        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-      </ul>
     </>
   );
 }
