062dc29dc2a01d399edd19f071119c0fdc505895
Grony <gr0ny.0017@gmail.com>
2023-02-26 03:11:37 +0300

    products

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -7,8 +7,10 @@ import ProductsIcon from "./icons/products.svg";
 import React from "react";
 
 export const firstLevelMenu: FirstLevelMenuItem[] = [
-  {route: '[type]', name: 'Курсы', icon: <CoursesIcon/>, id: TopLevelCategory.Courses},
+  {route: 'courses', name: 'Курсы', icon: <CoursesIcon/>, id: TopLevelCategory.Courses},
   {route: 'services', name: 'Сервисы', icon: <ServicesIcon/>, id: TopLevelCategory.Services},
   {route: 'books', name: 'Книги', icon: <BooksIcon/>, id: TopLevelCategory.Books},
   {route: 'products', name: 'Продукты', icon: <ProductsIcon/>, id: TopLevelCategory.Products},
 ];
+
+export const priceRu = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
