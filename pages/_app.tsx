446538beb18ebe094733d7eb725e0fcdd7b60077
Grony <gr0ny.0017@gmail.com>
2023-03-02 01:20:01 +0300

    Чекпоинт

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 import Head from 'next/head';
 import React from 'react';
+import {AppProps} from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	return (
-		<>
-		  <Head>
-			<title>Owl</title>
-			<link rel="icon" href="/favicon.ico" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
-		  </Head>
-		  <Component {...pageProps} />
-		</>
-	  );
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Owl</title>
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
