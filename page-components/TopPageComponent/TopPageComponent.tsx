f71c9e73504353288b75c2df23bcdff40cfed4a1
Grony <gr0ny.0017@gmail.com>
2023-03-01 17:46:06 +0300

    16

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,24 +1,31 @@
 import {TopPageComponentProps} from './TopPageComponent.props';
-import {Advantages, HhData, Htag, P, Tag} from '../../components';
+import {Advantages, HhData, Htag, Product, Sort, Tag} from '../../components';
 import styles from './TopPageComponent.module.css';
 import {TopLevelCategory} from '../../interfaces/page.interface';
+import {SortEnum} from '../../components/Sort/Sort.props';
+import {useReducer} from 'react';
+import {sortReducer} from './sort.reducer';
 
 export const TopPageComponent = ({page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
+  const [{products: sortedProducts, sort}, dispathSort] = useReducer(sortReducer, {products, sort: SortEnum.Rating});
+
+  const setSort = (sort: SortEnum) => {
+    dispathSort({type: sort});
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>
-        <Htag tag={'h1'}>{page.title}</Htag>
-        {products && <Tag color={'grey'} size={'m'}>{products.length}</Tag>}
-        <span>Сортировка</span>
+        <Htag tag='h1'>{page.title}</Htag>
+        {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+        <Sort sort={sort} setSort={setSort}/>
       </div>
-
       <div>
-        {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+        {sortedProducts && sortedProducts.map(p => (<Product key={p._id} product={p}/>))}
       </div>
-
       <div className={styles.hhTitle}>
-        <Htag tag={'h2'}>Вакансии - {page.title}</Htag>
-        <Tag color={'red'} size={'m'}>hh.ru</Tag>
+        <Htag tag='h2'>Вакансии - {page.category}</Htag>
+        <Tag color='red' size='m'>hh.ru</Tag>
       </div>
       {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>}
       {page.advantages && page.advantages.length > 0 &&
@@ -27,9 +34,11 @@ export const TopPageComponent = ({page, products, firstCategory}: TopPageCompone
           <Advantages advantages={page.advantages}/>
         </>
       }
-      {page.seoText && <P>{page.seoText}</P>}
+      {page.seoText &&
+        <div className={styles.seo} dangerouslySetInnerHTML={{__html: page.seoText}}/>
+      }
       <Htag tag={'h2'}>Ролучаемые навыки</Htag>
-      {page.tags.map(tag => <Tag key={tag} color={'primary'}>{tag}</Tag> )}
+      {page.tags.map(tag => <Tag key={tag} color={'primary'}>{tag}</Tag>)}
     </div>
   );
 };
